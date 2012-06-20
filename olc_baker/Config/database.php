4c3bac56026cbb00a67b578cecd69a9e6dc6c5ed
kiang <kiange@gmail.com>
2012-06-20 11:43:44 +0800

    upgrade locations

---
+++ olc_baker/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => 'FinjonKiang',
+		'database' => 'kiang_olc_baker',
+		'encoding' => 'utf8'
+	);
+}
